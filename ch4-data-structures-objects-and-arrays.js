34620a040437edba8ddde1059a7dd04b3f148850
Martin Kuna <martinkuna@users.noreply.github.com>
2023-02-26 02:01:23 -0700

    ch4 wip add sum

--- ch4-data-structures-objects-and-arrays.js
+++ ch4-data-structures-objects-and-arrays.js
@@ -13,16 +13,17 @@ console.log("\n\tv1 sum of a range");
         return arr;
     }
 
-    // TODO: add sum function
     let sum = arr => {
-        let total = 0;
-
-        return total;
+        if (Array.isArray(arr))
+            return arr.reduce((accumulator, currentValue) => accumulator + currentValue);
+        return null;
     }
-
+    
     console.log(range(1,10));
+    console.log(sum("not an array"));
+    console.log(sum(range(1,10)));
     console.log(range(1,10,2));
+    console.log(sum(range(1,10,2)));
     // console.log(range(5, 2, -1));
     // console.log(range(5, 2));
-    console.log(sum(range(1,10)));
 }
